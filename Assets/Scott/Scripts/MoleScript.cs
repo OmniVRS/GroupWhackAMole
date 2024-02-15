02df3c76e9c50193343dda4cb95a0f2a85c05c13
OmniVRS <scottkimball01@gmail.com>
2024-02-15 09:08:27 -0800

    Update MoleScript.cs

--- Assets/Scott/Scripts/MoleScript.cs
+++ Assets/Scott/Scripts/MoleScript.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public class MoleScript : Clickable
 {
+    private Animator animator;
+    private bool isClicked;
+    public float actualDifficulty;
+
+    protected override void Start()
+    {
+        base.Start();
+        animator = GetComponent<Animator>();
+        actualDifficulty = 3;
+        Invoke("LifeTime", actualDifficulty);
+    }
+
     protected override void GotClicked()
     {
-        
+        isClicked = true;
+        animator.SetBool("isDead", true);
+        Invoke("KillIt", actualDifficulty);
+    }
+
+    void LifeTime()
+    {
+        if (!isClicked)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void KillIt()
+    {
+        Destroy(gameObject);
     }
 }
